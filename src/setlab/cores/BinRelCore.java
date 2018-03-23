a8bbc0ad8fda2f6033c02822cf0dae218a110ed2
Eduard <Eduard@EdwardsPersonalLaptop>
2018-03-23 14:21:40 +0200

    - метод обработки строки

--- src/setlab/cores/BinRelCore.java
+++ src/setlab/cores/BinRelCore.java
@@ -2,7 +2,8 @@ package setlab.cores;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import setlab.cores.SetCore;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import setlab.cores.SetCore.SetObj;
 
 public class BinRelCore {
@@ -27,6 +28,7 @@ public class BinRelCore {
         public String toString() {
             return "(" + x + "," + y + ")";
         }
+
     }
 
     public static class BinRel extends HashSet<BinEl> {
@@ -38,32 +40,54 @@ public class BinRelCore {
         }
 
         private ArrayList<String> getElementFromLine(String line) {
-            return new ArrayList<String>();
+            ArrayList<String> list = new ArrayList<>();
+
+            String pattern = "[\\050](\\p{Alnum}{1,}[\\054]\\p{Alnum}{1,})[\\051]";
+            Pattern r = Pattern.compile(pattern);
+
+            boolean fl = true;
+            while (fl) {
+                Matcher m = r.matcher(line);
+                if (m.find()) {
+                    list.add(m.group(1));
+                    if (!line.isEmpty()) {
+                        line = line.substring(m.end());
+                    }
+                } else {
+                    fl = false;
+                }
+            }
+
+            return list;
         }
-        
+
         public BinRel(String name, String line) {
             this.name = name;
             for (String s : getElementFromLine(line)) {
                 this.add(new BinEl(s));
             }
         }
+
     }
-    
+
     public static SetObj D(BinRel r) {
         return new SetObj("D");
     }
+
     public static SetObj E(BinRel r) {
         return new SetObj("D");
     }
+
     public static SetObj O(BinRel r) {
         return SetCore.Union(D(r), E(r));
     }
-    
+
     public static BinRel Reverse(BinRel r) {
         return new BinRel("invR");
     }
-    
+
     public static BinRel Composer(BinRel r) {
         return new BinRel("CombR");
     }
+
 }
