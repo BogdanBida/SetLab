f69fde32676a82ff167982e0e08b6ba952cd02b6
Eduard <Eduard@EdwardsPersonalLaptop>
2018-03-23 15:21:14 +0200

    - объявленный компоненты - созданы главные методы

--- src/setlab/controller/MainWindowController.java
+++ src/setlab/controller/MainWindowController.java
@@ -7,12 +7,36 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import setlab.cores.SetCore;
 import setlab.cores.SetCore.SetObj;
 
 public class MainWindowController implements Initializable {
     
     public static HashMap<String,SetObj> MapOfSets = new HashMap<>();
+    
+    @FXML
+    private Tab tab_set;
+
+    @FXML
+    private ListView<?> set_listView;
+    
+    @FXML
+    private TextArea set_area;
+
+    @FXML
+    private TextField set_field;
+    
+    @FXML
+    private Tab tab_binRel;
+
+    @FXML
+    private Tab tab_comb;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -22,6 +46,13 @@ public class MainWindowController implements Initializable {
         System.out.println(SetCore.Intersect(A, B).toString());
         System.out.println(SetCore.Diff(A, B).toString());
         System.out.println(SetCore.SymmetricDiff(A, B).toString());
+    }
+    
+    @FXML
+    public void getCommand(){
+        if (!set_field.getText().isEmpty()) {
+            
+        }
     }
     
     @FXML
