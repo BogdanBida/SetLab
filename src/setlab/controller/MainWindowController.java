825441bbcbe915a0bc78eb058c6f1cdc61c0daf8
Eduard <Eduard@EdwardsPersonalLaptop>
2018-03-23 17:04:34 +0200

    - добавленны новые компоненты

--- src/setlab/controller/MainWindowController.java
+++ src/setlab/controller/MainWindowController.java
@@ -7,14 +7,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import setlab.cores.SetCore;
+import javafx.scene.web.WebView;
 import setlab.cores.SetCore.SetObj;
-import setlab.cores.CombCore;
-import setlab.cores.BinRelCore;
 
 public class MainWindowController implements Initializable {
     
@@ -34,15 +33,28 @@ public class MainWindowController implements Initializable {
     
     @FXML
     private Tab tab_binRel;
-
+    
     @FXML
     private Tab tab_comb;
-    
-    
-    
+
+    @FXML
+    private WebView comb_webView;
+
+    @FXML
+    private TextArea comb_infoAccept;
+
+    @FXML
+    private Button comb_infoAccept_noBtn;
+
+    @FXML
+    private Button comb_infoAccept_yesBtn;
+
+    @FXML
+    private Button comb_infoAccept_backBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+
     }
     
     @FXML
