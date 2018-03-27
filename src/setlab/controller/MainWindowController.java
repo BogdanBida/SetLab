0b78848259169208a82235c37009b85f1ab1e1c0
Eduard <Eduard@EdwardsPersonalLaptop>
2018-03-27 08:47:47 +0200

    - настроено ветвлени

--- src/setlab/controller/MainWindowController.java
+++ src/setlab/controller/MainWindowController.java
@@ -1,12 +1,10 @@
 package setlab.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,22 +13,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebView;
 import setlab.cores.SetCore.SetObj;
-import setlab.cores.BinRelCore.BinRel;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-import setlab.cores.BinRelCore;
-import setlab.cores.CombCore;
 
 public class MainWindowController implements Initializable {
 
@@ -55,19 +39,10 @@ public class MainWindowController implements Initializable {
     private Tab tab_comb;
 
     @FXML
-    private TextArea binrel_area;
-
-    @FXML
-    private TextField binrel_field;
-
-    @FXML
-    private Button binrel_analysis;
-
-    @FXML
     private WebView comb_webView;
 
     @FXML
-    private TextArea comb_infoAccept;
+    private WebView comb_infoAcceptWebView;
 
     @FXML
     private Button comb_infoAccept_noBtn;
@@ -78,80 +53,87 @@ public class MainWindowController implements Initializable {
     @FXML
     private Button comb_infoAccept_backBtn;
 
-    DocumentBuilder builder;
-    Document doc;
-    Element element;
-    Node node;
-    Node t;
+    SimpleStringProperty string = new SimpleStringProperty();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // ---------------------------------------------- COMBINATORICS PAGE ---
-        comb_webView.setContextMenuEnabled(false);
-        comb_infoAccept_backBtn.setDisable(true);
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            builder = factory.newDocumentBuilder();
-            doc = builder.parse(new File("src/setlab/combAlg.xml"));
-            element = doc.getDocumentElement();
-
-        } catch (IOException | ParserConfigurationException | DOMException | SAXException ex) {
-            System.err.println("Error");
-        }
-
-        // YES
-        comb_infoAccept_yesBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent) -> {
-            comb_infoAccept_backBtn.setDisable(false);
-
-            comb_infoAccept.setText(node.getNodeValue());
-        });
-        // NO
-        comb_infoAccept_noBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent) -> {
-            comb_infoAccept_backBtn.setDisable(false);
-
-            comb_infoAccept.setText(node.getNodeValue());
-        });
-        // BACK
-        comb_infoAccept_backBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent) -> {
-            node = node.getParentNode().getPreviousSibling();
-            comb_infoAccept.setText(node.getNodeValue());
-
-            if (node.getParentNode().getPreviousSibling() == null) {
-                comb_infoAccept_backBtn.setDisable(true);
+        string.addListener((observable, oldValue, newValue) -> {
+            String massage = "";
+            switch (string.getValue()) {
+                case "start":
+                    massage = "Важен ли порядок расположения элементов в КК?";
+                    comb_infoAccept_backBtn.setDisable(true);
+                    comb_infoAccept_yesBtn.setDisable(false);
+                    comb_infoAccept_noBtn.setDisable(false);
+                    break;
+                case "start;yeah":
+                    massage = "Все ли элементы множества А входят в КК?";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(false);
+                    comb_infoAccept_noBtn.setDisable(false);
+                    break;
+                case "start;yeah;yeah":
+                    massage = "Есть ли в КК повторения элементов?";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(false);
+                    comb_infoAccept_noBtn.setDisable(false);
+                    break;
+                case "start;yeah;yeah;nope":
+                    massage = "Перестановки без повторений из n элементов";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                case "start;yeah;yeah;yeah":
+                    massage = "Перестановки с повторениями из n элементов по m с заданой спецификацией";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                case "start;yeah;nope":
+                    massage = "Есть ли в КК повторения элементов?";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(false);
+                    comb_infoAccept_noBtn.setDisable(false);
+                    break;
+                case "start;yeah;nope;yeah":
+                    massage = "Размещения с повторениями из n элементов по m";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                case "start;yeah;nope;nope":
+                    massage = "Размещения без повторений из n элементов по m";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                case "start;nope":
+                    massage = "Есть ли в КК повторения элементов?";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(false);
+                    comb_infoAccept_noBtn.setDisable(false);
+                    break;
+                case "start;nope;yeah":
+                    massage = "Сочетания с повторениями из n элементов по m";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                case "start;nope;nope":
+                    massage = "Сочетания без повторений из n элементов по m";
+                    comb_infoAccept_backBtn.setDisable(false);
+                    comb_infoAccept_yesBtn.setDisable(true);
+                    comb_infoAccept_noBtn.setDisable(true);
+                    break;
+                default:
+                    System.out.println("Ошибка почему-то");
             }
+
+            String html = "<html><h4>" + massage + "</h4></html>";
+            comb_infoAcceptWebView.getEngine().loadContent(html);
         });
-        // ---------------------------------------------------------------------
-
-    }
-
-    @FXML
-    public void analisisBinRel() {
-        BinRel A = new BinRel("A", "((2,1) (4,2) (1,3) (2,3))");
-        StringBuilder res = new StringBuilder(A + "\n");
-        res.append(BinRelCore.D(A));
-        res.append("\n");
-        res.append(BinRelCore.E(A));
-        res.append("\n");
-        res.append(BinRelCore.O(A));
-        res.append("\n");
-        res.append(BinRelCore.Ident(BinRelCore.O(A)));
-        res.append("\n");
-        res.append(BinRelCore.Reverse(A));
-        res.append("\n ___ \n");
-        res.append(BinRelCore.Refelex(A));
-        res.append("\n");
-        res.append(BinRelCore.AntiRefelex(A));
-        res.append("\n");
-        res.append(BinRelCore.Simetry(A));
-        res.append("\n");
-        res.append(BinRelCore.AntiSimetry(A));
-        res.append("\n");
-        res.append(BinRelCore.Asimetry(A));
-        res.append("\n");
-        res.append(BinRelCore.Transity(A));
-        res.append("\n");
-        binrel_area.setText(res.toString());
     }
 
     @FXML
@@ -163,7 +145,35 @@ public class MainWindowController implements Initializable {
 
     @FXML
     public void Manual() throws IOException {
+    }
 
+    @FXML
+    public void commandStart() {
+        string.set("start");
+    }
+
+    @FXML
+    public void commandYeah() {
+        String temp = string.getValue();
+        temp = temp + ";yeah";
+        
+        string.set(temp);
+    }
+
+    @FXML
+    public void commandNope() {
+        String temp = string.getValue();
+        temp = temp + ";yeah";
+
+        string.set(temp);
+    }
+
+    @FXML
+    public void commandBack() {
+        String temp = string.getValue();
+        temp = temp.substring(0, temp.length() - 5);
+
+        string.set(temp);
     }
 
     @FXML
@@ -171,11 +181,4 @@ public class MainWindowController implements Initializable {
         System.exit(0);
     }
 
-    /*
-        - Запрет на ввод текста в поля вывода
-        - Пункут меню "о программе" модальное окно с авторами, версией
-            датой созданий - текущей датой (Пример: 25.03.2018 - 01.01.2020)
-            почтой и.т.д.
-        - Найти иконки для бинарных отношений
-     */
 }
