27cfc7a4a978302a3303247d026c72b2ff933525
Eduard <Eduard@EdwardsPersonalLaptop>
2018-04-09 13:19:40 +0300

    - добавлена иконка

--- src/setlab/SetLab.java
+++ src/setlab/SetLab.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import setlab.controller.Setting;
 
@@ -18,6 +19,7 @@ public class SetLab extends Application {
         Scene scene = new Scene(root);
 
         primaryStage.setTitle(NAME_PROGRAM + Setting.VERSION);
+        primaryStage.getIcons().add(new Image(SetLab.class.getResourceAsStream("fxml/icon/SetLab.png")));
         
         primaryStage.setMinWidth(670);
         primaryStage.setMinHeight(70 + 440);
