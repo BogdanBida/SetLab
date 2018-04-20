07d796ab7b12edddaac6c985549adbfeb469622a
Bogdan <Bogdan@LAPTOP-CHTJCNUO>
2018-04-20 21:12:47 +0300

    - восстановление проекта

--- src/setlab/controller/FeedbackWindowController.java
+++ src/setlab/controller/FeedbackWindowController.java
@@ -10,9 +10,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+//import javax.mail.*;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
 
 public class FeedbackWindowController implements Initializable {
 
@@ -32,16 +32,16 @@ public class FeedbackWindowController implements Initializable {
     String EMAIL_password = "BeqhBE09";
     String EMAIL_host = "smtp.gmail.com";
     Properties props = new Properties();
-    Session session = null;
-    Transport transport = null;
+  //  Session session = null;
+  //  Transport transport = null;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        try {
-            initializeFeedBack();
-        } catch (NoSuchProviderException ex) {
-            ex.printStackTrace();
-        }
+//        try {
+//            initializeFeedBack();
+//        } catch (NoSuchProviderException ex) {
+//            ex.printStackTrace();
+//        }
     }
     
     @FXML
@@ -50,22 +50,22 @@ public class FeedbackWindowController implements Initializable {
     }
     
     public void sendMassege(String title, String text){
-        try {
-            transport.connect("smtp.gmail.com", EMAIL_login, EMAIL_password);
-            MimeMessage msg = new MimeMessage(session);
-
-            msg.setFrom(new InternetAddress(EMAIL_login));
-            InternetAddress[] address = {new InternetAddress(EMAIL_login)};
-            msg.setRecipients(Message.RecipientType.TO, address);
-            msg.setSubject(title);
-            msg.setSentDate(new Date());
-
-            msg.setText(text);
-
-            transport.sendMessage(msg, msg.getRecipients(Message.RecipientType.TO));
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
+//        try {
+//            transport.connect("smtp.gmail.com", EMAIL_login, EMAIL_password);
+//            MimeMessage msg = new MimeMessage(session);
+//
+//            msg.setFrom(new InternetAddress(EMAIL_login));
+//            InternetAddress[] address = {new InternetAddress(EMAIL_login)};
+//            msg.setRecipients(Message.RecipientType.TO, address);
+//            msg.setSubject(title);
+//            msg.setSentDate(new Date());
+//
+//            msg.setText(text);
+//
+//            transport.sendMessage(msg, msg.getRecipients(Message.RecipientType.TO));
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
         
     }
 
@@ -86,25 +86,25 @@ public class FeedbackWindowController implements Initializable {
         return res.toString();
     }
 
-    private void initializeFeedBack() throws NoSuchProviderException{
-        int MAX_LENGTH = 100;
-        fd_score.setText("0/" + MAX_LENGTH);
-        fd_titleField.textProperty().addListener((observable, oldValue, newValue) -> {
-            fd_score.setText(newValue.length() + "/" + MAX_LENGTH);
-            if (newValue.length() > MAX_LENGTH) {
-                fd_titleField.setText(oldValue);
-            }
-        });
-        
-        fd_btnSend.disableProperty().bind(fd_textArea.textProperty().isEmpty().or(fd_titleField.textProperty().isEmpty()));
-        
-        props.put("mail.smtp.host", EMAIL_login);
-        props.put("mail.smtp.ssl.enable", "smtps");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        
-        session = Session.getDefaultInstance(props);
-        transport = session.getTransport();
-    }
+//    private void initializeFeedBack() throws NoSuchProviderException{
+//        int MAX_LENGTH = 100;
+//        fd_score.setText("0/" + MAX_LENGTH);
+//        fd_titleField.textProperty().addListener((observable, oldValue, newValue) -> {
+//            fd_score.setText(newValue.length() + "/" + MAX_LENGTH);
+//            if (newValue.length() > MAX_LENGTH) {
+//                fd_titleField.setText(oldValue);
+//            }
+//        });
+//        
+//        fd_btnSend.disableProperty().bind(fd_textArea.textProperty().isEmpty().or(fd_titleField.textProperty().isEmpty()));
+//        
+//        props.put("mail.smtp.host", EMAIL_login);
+//        props.put("mail.smtp.ssl.enable", "smtps");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.debug", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        
+//        session = Session.getDefaultInstance(props);
+//        transport = session.getTransport();
+//    }
 }
